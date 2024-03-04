92108254b616dee9dc60eb97e012cbea9e2f197b
somjunggg <napassornbns@gmail.com>
2024-03-04 08:12:44 +0700

    config firebase

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyADoSCV6ihLHRaJ1v95wf3JF1qcJnC0s_Y",
+    authDomain: "examitopplus.firebaseapp.com",
+    projectId: "examitopplus",
+    storageBucket: "examitopplus.appspot.com",
+    messagingSenderId: "9280757646",
+    appId: "1:9280757646:web:6b3033e8312b6e83034edf",
+    measurementId: "G-L17X834BB4"
+  }
+};
